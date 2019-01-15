a92e1b60a880c11b6a071896991a4ccaa9e743b3
hamed <eppad.Fakouri@gmail.com>
2019-01-15 21:17:39 +0330

    create educationLevelService with controller

---
+++ Tcm.Application.Contract/EducationLevels/IEducationLevelService.cs
@@ -0,0 +1,18 @@
+﻿using Framework.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tcm.Domain.Model;
+
+namespace Tcm.Application.Contract.EducationLevels
+{
+
+    public interface IEducationLevelService:IApplicationService
+    {
+        void Add(EducationLevelAddDto educationLevelDto);
+
+        EducationLevelDto Get(short Id);
+
+
+    }
+}
