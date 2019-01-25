458e82701378c2f219717b3d6315c4e30a4fc6dc
HealthyStyle <moha.noorani88@gmail.com>
2019-01-25 16:32:46 +0330

    fix permission.service
    bypass jsonconfig camelcase return type
    fix httpservice

--- Tcm.Interface.WebUi/src/app/authentication/services/permission.service.ts
+++ Tcm.Interface.WebUi/src/app/authentication/services/permission.service.ts
@@ -8,9 +8,9 @@ import { TaskType, SubjectType, Permission, RoleType } from 'src/app/core/models
 export class PermissionService {
 
 
-  public userPermissions :Array<Permission>;
-  public hasPermission:boolean =false;
-  public role :RoleType;
+  public userPermissions: Array<Permission>;
+  public hasPermission: boolean = false;
+  public role: RoleType;
   constructor() {
     this.userPermissions = new Array<Permission>();
 
@@ -18,34 +18,33 @@ export class PermissionService {
   }
 
 
-   GetPermissions(value:string){
+  GetPermissions(value: string) {
 
     this.role = RoleType[value];
-    this.userPermissions.push(new Permission(SubjectType.corporate,TaskType.add));
-    // this.userPermissions.push(new Permission(SubjectType.corporate,TaskType.delete));
-    this.userPermissions.push(new Permission(SubjectType.corporate,TaskType.edit));
+    this.userPermissions.push(new Permission(SubjectType.educationlevel, TaskType.add));
+    this.userPermissions.push(new Permission(SubjectType.educationlevel, TaskType.delete));
+    this.userPermissions.push(new Permission(SubjectType.educationlevel, TaskType.edit));
     // this.userPermissions.push(new Permission(SubjectType.dashboard,TaskType.update));
-   }
+  }
 
-   HasPermission(subject:SubjectType,task:TaskType):boolean{
-  
-  
-    let subPermission :boolean =false;
-    let taskPermission :boolean =false;
-    
+  HasPermission(subject: SubjectType, task: TaskType): boolean {
 
-    if(subject){
-         this.userPermissions.forEach(sub => { if(sub.subject==subject){ subPermission = true}} )
-    }else{
-     subPermission =true;
 
+    let subPermission: boolean = false;
+    let taskPermission: boolean = false;
+
+    this.GetPermissions('Admin');
+    if (subject) {
+      this.userPermissions.forEach(sub => { if (sub.subject == subject) { subPermission = true } })
+    } else {
+      subPermission = true;
     }
 
-    if(task){
-      this.userPermissions.forEach(sub => { if(sub.subject==subject && sub.task == task){ taskPermission = true}} )
+    if (task) {
+      this.userPermissions.forEach(sub => { if (sub.subject == subject && sub.task == task) { taskPermission = true } })
 
-    }else{
-      taskPermission=true;
+    } else {
+      taskPermission = true;
     }
 
     return subPermission && taskPermission;
@@ -54,7 +53,7 @@ export class PermissionService {
 
   //  this._task = TaskType[this.task];
   //  this._subject = SubjectType[this.subject];
-   
+
   //    if(!this._subject){
   //    if(this.router.snapshot.data.permission)
   //             {
@@ -65,5 +64,5 @@ export class PermissionService {
   //    this._subject = SubjectType[this.subject];
   //    }
   //    }
-    
+
 }
--- Tcm.Interface.WebUi/src/app/shared/components/appGrid/app-grid.component.ts
+++ Tcm.Interface.WebUi/src/app/shared/components/appGrid/app-grid.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Pagination } from 'src/app/core/models/pagination';
 import { Pair, TaskType, SubjectType } from 'src/app/core/models';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/authentication/services';
 
-import { PairType } from 'src/app/core/models/pair-type.enum';
 import { PermissionService } from 'src/app/authentication/services/permission.service';
 
 
@@ -14,127 +12,82 @@ import { PermissionService } from 'src/app/authentication/services/permission.se
   selector: 'app-grid',
   templateUrl: './app-grid.component.html'
 })
-export class AppGridComponent implements  OnInit , OnChanges ,DoCheck ,AfterContentInit ,AfterContentChecked ,AfterViewInit ,AfterViewChecked {
- 
-  @Input()  items :any[];
-  @Input()  dictionary:Array<Pair>;
-  @Input()  subject:string;
-  @Input()  pagination:Pagination;
-  @Input()  editable: boolean =false;
-  @Input()  removable: boolean =false;
+export class AppGridComponent implements OnInit {
+
+  @Input() items: any[];
+  @Input() dictionary: Array<Pair>;
+  @Input() subject: string;
+  @Input() pagination: Pagination;
+  @Input() editable: boolean = false;
+  @Input() removable: boolean = false;
 
   @Output() onSort = new EventEmitter();
   @Output() onPageChanged = new EventEmitter();
   @Output() onEdit = new EventEmitter();
   @Output() onRemove = new EventEmitter();
-   
-  showGrid:boolean =false;
 
-     
-  constructor(private router:Router,private permissionService:PermissionService) { 
-
-   console.log("constructor");
+  showGrid: boolean = false;
 
 
-
-
-
-
-  }
-
- 
-
-
-  ngOnChanges(){
-
-console.log("ngOnChanges");
-
-  }
+  constructor(private router: Router, private permissionService: PermissionService) { }
 
   ngOnInit() {
-  
-    
-this.showGrid =true;
-let _subject = SubjectType[this.subject];
-if(this.permissionService.HasPermission(_subject, TaskType.edit) && this.editable){
 
- this.editable = true;
+    this.showGrid = true;
+    let _subject = SubjectType[this.subject];
+    if (this.permissionService.HasPermission(_subject, TaskType.edit) && this.editable) {
 
-}else{
-  this.editable =false;
-}
+      this.editable = true;
 
-if(this.permissionService.HasPermission(_subject,TaskType.delete) && this.editable){
+    } else {
+      this.editable = false;
+    }
 
-this.removable=true;
+    if (this.permissionService.HasPermission(_subject, TaskType.delete) && this.removable) {
+
+      this.removable = true;
 
 
-}else{
-  this.removable =false;
-}
+    } else {
+      this.removable = false;
+    }
     console.log("ngOnInit");
 
   }
 
-ngDoCheck(){
-  console.log("ngDoCheck");
-
-}
-
-ngAfterContentInit(){
-  console.log("ngAfterContentInit");
-
-}
-
-ngAfterContentChecked(){
-  console.log("ngAfterContentChecked"); //show grid header
-
-}
-
-ngAfterViewInit(){
-  console.log("ngAfterViewInit");
-
-}
-
-ngAfterViewChecked(){
-  
-  console.log("ngAfterViewChecked");
-
-}
-
-  pageChanged(event:any):void{
-    this.onPageChanged.emit(event); 
+  pageChanged(event: any): void {
+    this.onPageChanged.emit(event);
   }
 
-  getIndex(value:number):number{
-    return ((this.pagination.currentPage -1 ) * this.pagination.itemsPerPage) + value + 1 ;
+  getIndex(value: number): number {
+    return ((this.pagination.currentPage - 1) * this.pagination.itemsPerPage) + value + 1;
   }
 
-  edit(value:any){
+  edit(value: any) {
     this.onEdit.emit(value);
 
   }
-  remove(value:any){
+  remove(value: any) {
     this.onRemove.emit(value);
 
   }
 
-  sort(value:any){
-  
+  sort(value: any) {
 
-     if(this.pagination.orderBy==value){
 
-      if(this.pagination.orderByType=="DESC"){
-        this.pagination.orderByType="ASC";
-      } else{
-        this.pagination.orderByType="DESC"
+    if (this.pagination.orderBy == value) {
+
+      if (this.pagination.orderByType == "DESC") {
+        this.pagination.orderByType = "ASC";
+      } else {
+        this.pagination.orderByType = "DESC"
       }
-     }
-     else{
+    }
+    else {
       this.pagination.orderBy = value;
       this.pagination.orderByType = "ASC";
-     }
+    }
     this.onSort.emit(this.pagination);
-    
-  }  
+
+  }
 }
